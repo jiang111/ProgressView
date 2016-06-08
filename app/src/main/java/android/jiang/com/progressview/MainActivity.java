25b27e51361243db5e65028b8594f9a6696d186e
jyuesong <jyuesong@gmail.com>
2016-06-08 10:18:10 +0800

    add demo

--- app/src/main/java/android/jiang/com/progressview/MainActivity.java
+++ app/src/main/java/android/jiang/com/progressview/MainActivity.java
@@ -1,44 +1,68 @@
 package android.jiang.com.progressview;
 
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
 
-    Handler mHandler = new Handler();
-    int position = 0;
+    List<android.support.v4.app.Fragment> mLists = new ArrayList<>();
+    private List<ProgressView.Model> mModels;
+    private ProgressView mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ProgressView view = (ProgressView) findViewById(R.id.ffff);
+        mView = (ProgressView) findViewById(R.id.ffff);
 
-        final List<ProgressView.Model> models = new ArrayList<>();
+        mModels = new ArrayList<>();
 
-        models.add(new ProgressView.Model("确认密码", ProgressView.STARTING));
-        models.add(new ProgressView.Model("输入邮箱", ProgressView.AFTER));
-        models.add(new ProgressView.Model("再次输入", ProgressView.AFTER));
-        models.add(new ProgressView.Model("最终完成", ProgressView.AFTER));
+        mLists.add(TestFragment.newInstance(0, "确认密码"));
+        mLists.add(TestFragment.newInstance(1, "输入邮箱"));
+        mLists.add(TestFragment.newInstance(2, "再次输入"));
+        mLists.add(TestFragment.newInstance(3, "最终完成"));
 
-        view.setData(models);
+        mModels.add(new ProgressView.Model("确认密码", ProgressView.STARTING));
+        mModels.add(new ProgressView.Model("输入邮箱", ProgressView.AFTER));
+        mModels.add(new ProgressView.Model("再次输入", ProgressView.AFTER));
+        mModels.add(new ProgressView.Model("最终完成", ProgressView.AFTER));
+
+        mView.setData(mModels);
+        switchFragment(null, mLists.get(0));
 
 
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                models.get(position).state = ProgressView.BEFORE;
-                models.get(position + 1).state = ProgressView.STARTING;
-                position++;
-                view.setData(models);
+    }
+
+    public void switchFragment(Fragment from, Fragment to) {
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        if (from == null) {
+            if (to != null) {
+                transaction.replace(R.id.id_content, to);
+                transaction.commit();
             }
-        }, 3000);
+        } else {
+            if (!to.isAdded()) {
+                transaction.hide(from).add(R.id.id_content, to).commit();
+            } else {
+                transaction.hide(from).show(to).commit();
+            }
 
+        }
 
+    }
+
+    public void setPosition(int position) {
+        switchFragment(mLists.get(position), mLists.get(position + 1));
+        mModels.get(position).state = ProgressView.BEFORE;
+        mModels.get(position + 1).state = ProgressView.STARTING;
+        mView.setData(mModels);
     }
 }
---
+++ app/src/main/java/android/jiang/com/progressview/TestFragment.java
@@ -0,0 +1,79 @@
+/**
+ * created by jiang, 16/6/8
+ * Copyright (c) 2016, jyuesong@gmail.com All Rights Reserved.
+ * *                #                                                   #
+ * #                       _oo0oo_                     #
+ * #                      o8888888o                    #
+ * #                      88" . "88                    #
+ * #                      (| -_- |)                    #
+ * #                      0\  =  /0                    #
+ * #                    ___/`---'\___                  #
+ * #                  .' \\|     |# '.                 #
+ * #                 / \\|||  :  |||# \                #
+ * #                / _||||| -:- |||||- \              #
+ * #               |   | \\\  -  #/ |   |              #
+ * #               | \_|  ''\---/''  |_/ |             #
+ * #               \  .-\__  '-'  ___/-. /             #
+ * #             ___'. .'  /--.--\  `. .'___           #
+ * #          ."" '<  `.___\_<|>_/___.' >' "".         #
+ * #         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       #
+ * #         \  \ `_.   \_ __\ /__ _/   .-` /  /       #
+ * #     =====`-.____`.___ \_____/___.-`___.-'=====    #
+ * #                       `=---='                     #
+ * #     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   #
+ * #                                                   #
+ * #               佛祖保佑         永无BUG              #
+ * #                                                   #
+ */
+
+package android.jiang.com.progressview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by jiang on 16/6/8.
+ */
+public class TestFragment extends Fragment {
+
+
+    public static TestFragment newInstance(int position, String name) {
+
+        Bundle args = new Bundle();
+        args.putInt("position", position);
+        args.putString("name", name);
+        TestFragment fragment = new TestFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        final int position = getArguments().getInt("position");
+        String name = getArguments().getString("name");
+        Button btn = new Button(container.getContext(), null);
+        btn.setGravity(Gravity.CENTER);
+        btn.setText("当前状态:" + name);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity) getActivity()).setPosition(position);
+            }
+        });
+        return btn;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
